0986b5ce72c3a9bf1837853ff7910999dc3a1891
wasawatkp <wasawat.upper@gmail.com>
2024-12-06 22:18:11 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 char before(char x){
 	//Write your function definition here
+
+
+	if(x >= 'A' && x <= 'Z') {
+		if(x == 'A'){ return x ='Z'; }
+		else { return x = x-1;}
+		
+		}
+	else return '0' ;
+	
+
+	
 }
 
 int main(){
